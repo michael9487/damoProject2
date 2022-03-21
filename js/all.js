58fe3eaad3f8013f10cc9228d5626b1c0dfe68cf
michael5278 <39005630+michael5278@users.noreply.github.com>
2022-03-21 18:50:19 +0800

    finalEdition

--- js/all.js
+++ js/all.js
@@ -1,6 +1,56 @@
+//swiper輪播
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    speed:2000,
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    effect:'cube',
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+
+///漢堡選單
 $(document).ready(function () {
     $('.showmenu').on('click',function(e){
         e.preventDefault();
         $('body').toggleClass('menu-show');
     });
+});
+
+
+//top回最上方效果
+$('.backtotop a').click(function (e) { 
+    e.preventDefault();
+    $('html,body').animate({
+        scrollTop:0
+    },1000);
+});
+
+$('.scrolltoTips').click(function (e) { 
+    e.preventDefault();
+    $('html,body').animate({
+        scrollTop: $('.tips').offset().top
+    },1000);
+});
+$('.scrolltoChef').click(function (e) { 
+    e.preventDefault();
+    $('html,body').animate({
+        scrollTop: $('.chefToday').offset().top
+    },1000);
+});
+$('.scrolltoReserve').click(function (e) { 
+    e.preventDefault();
+    $('html,body').animate({
+        scrollTop: $('.reserve').offset().top
+    },1000);
 });
